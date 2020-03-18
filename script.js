c23aac5b1a0e983793e20f5edacc0c5e1f12e626
Johan Back <johan.backs@gmail.com>
2020-03-18 11:28:27 -0300

    add comida

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
   y: 8 * box
 }
 let direction = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1 ) * box,
+  y: Math.floor(Math.random() * 15+ 1 ) * box 
+};
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -21,6 +25,11 @@ function criarCobrinha() {
   }
 }
 
+
+function drawFood() {
+  context.fillStyle = "red"
+  context.fillRect(food.x, food.y ,box , box)
+}
 document.addEventListener("keydown" , update) ;
 
 function update(event) {
@@ -36,8 +45,10 @@ function iniciarJogo() {
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y >15 * box && direction == "down" ) snake[0].y = 0 ;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box ; 
+
   criarBG();
   criarCobrinha();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
